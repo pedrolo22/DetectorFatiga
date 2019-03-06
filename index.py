8186648a539596a311ca932314330b81a651443c
pedrolo22 <pedro.gpaterna@yahoo.com>
2019-03-06 08:56:54 +0100

    Guardado de capturas

--- index.py
+++ index.py
@@ -45,86 +45,77 @@ def detect_eyes_OpenCV(imagen):
 	eye2=eyes[1,:]
 	ex1,ey1,ew1,eh1=eye1
 	ex2,ey2,ew2,eh2=eye2
-	cv2.rectangle(imagen,(ex1,ey1),(ex1+ew1,ey1+eh1),(0,255,0),1)
-	cv2.rectangle(imagen,(ex2,ey2),(ex2+ew2,ey2+eh2),(0,255,0),1)
+	#cv2.rectangle(imagen,(ex1,ey1),(ex1+ew1,ey1+eh1),(0,255,0),1)
+	#cv2.rectangle(imagen,(ex2,ey2),(ex2+ew2,ey2+eh2),(0,255,0),1)
 	return imagen,eye1,eye2,detect_eyes
 
-def elim_ruido(imagen):
-	dim=3
-	filtro=np.ones((dim,dim),np.float32)/(dim*dim)
-	im_suav=cv2.filter2D(imagen,-1,filtro)
-	return im_suav
+def ellipse_aprox(imagen):
+	img_suav=cv2.GaussianBlur(imagen,(3,3),0)
+	img_equal=cv2.equalizeHist(img_suav)
+	img_bw=cv2.adaptiveThreshold(img_equal, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,15,2)
+	contours,hierarchy=cv2.findContours(img_bw,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	#cv2.drawContours(img_bw,contours,-1,(0,255,0),3)
+	#cv2.ellipse(imagen,(0,255,0),2)
+	cv2.imshow('Ojo binarizado',img_equal)
 
+def proy_bin(imagen):
+	img_suav=cv2.GaussianBlur(imagen,(3,3),0)
+	img_equal=cv2.equalizeHist(img_suav)
+	proy_ver=np.sum(255-img_equal,0)
+	proy_hor=np.sum(255-img_equal,1)
+	index_ver=sum(proy_ver>3500)
+	index_hor=sum(proy_hor>3500)
+	apertura=float(index_hor)/float(index_ver)
+	print(index_ver)
+	print(index_hor)
+	print(apertura)
 
-def procesado_ojo(ojo):
-	ojo_bw=cv2.cvtColor(ojo,cv2.COLOR_BGR2GRAY)
-	ojo_suav=elim_ruido(ojo_bw)
-	#ojo_equal=cv2.equalizeHist(ojo_suav)
-	umbral,ojo_umbr=cv2.threshold(ojo_suav,70,255,cv2.THRESH_BINARY_INV)
-	kernel = np.ones((5,5),np.uint8)
-	ojo_erod=cv2.erode(ojo_umbr, kernel, 5)
-	proye_hor=np.sum(ojo_erod,0)
-	proye_ver=np.sum(ojo_erod,1)
-	umbral_hor=0.8*max(proye_hor)
-	umbral_ver=0.3*max(proye_ver)
-	index_hor=(proye_hor>umbral_hor)
-	result=index_hor*proye_ver
-	index_ver=(proye_ver>umbral_ver)
-	pixeles_hor=np.float(sum(index_hor))
-	pixeles_ver=np.float(sum(index_ver))
-	apertura=pixeles_ver/pixeles_hor
-
-	plt.subplot(241)
-	plt.imshow(ojo,cmap='gray')
-	plt.title('Ojo original')
-	plt.xticks([]), plt.yticks([])
-	plt.subplot(242)
-	plt.imshow(ojo_suav,cmap='gray')
-	plt.title('Ojo tras suavizar')
-	plt.subplot(243)
-	plt.imshow(ojo_suav,cmap='gray')
-	plt.title('Ojo tras ecualizar')
-	plt.subplot(244)
-	plt.imshow(ojo_umbr,cmap='gray')
-	plt.title('Ojo tras umbralizar')
-	plt.subplot(245)
-	plt.imshow(ojo_erod,cmap='gray')
-	plt.title('Ojo tras erosion')
-	plt.subplot(246)
-	plt.plot(proye_hor)
-	plt.title('Proyeccion horizontal')
-	plt.subplot(247)
-	plt.plot(proye_ver)
-	plt.title('Proyeccion vertical')
-	plt.subplot(248)
-	plt.plot(result)
-	plt.title('Result')
+	plt.subplot(131)
+	plt.plot(proy_hor)
+	plt.title('proy_hor')
+	plt.subplot(132)
+	plt.plot(proy_ver)
+	plt.title('proy_ver')
+	plt.subplot(133)
+	plt.imshow(img_equal,cmap='gray')
+	plt.title('ojo')
 	plt.show()
-
 	return apertura
 
 start_time=t.time()
-im=cv2.imread('images/ojos_abiertos_HD.jpg',0)
+im=cv2.imread('images/ojos_entre_HD.jpg',0)
 im_resize=cv2.UMat(cv2.resize(im,(320,180)))
 
 face_UMat,fx,fy,fw,fh,fdetect=detect_careto_OpenCV(im_resize)
 face=cv2.UMat.get(face_UMat)
 cv2.imshow('pedrolo',im_resize)
+cv2.imwrite('./capturas/im_resize.jpg',face)
 face_time=t.time()-start_time
 print('Tiempo ejecucion cara',face_time)
 
 ROI_face=face[fy:fy+fh , fx:fx+fw]
 cv2.imshow('face',ROI_face)
+cv2.imwrite('./capturas/face.jpg',ROI_face)
 dimY,dimX=ROI_face.shape
 print('Tamano cara',dimY,dimX)
 ROI_eyes=ROI_face[int(dimX*0.25):int(dimX*0.55),int(dimY*0.1):int(dimY*0.9)]
 ROI_mouth=ROI_face[int(dimX*0.7):int(dimX*0.95),int(dimY*0.2):int(dimY*0.8)]
 cv2.imshow('mouth', ROI_mouth)
+cv2.imwrite('./capturas/mouth.jpg',ROI_mouth)
+print('Tamano boca',ROI_mouth.shape)
 print('Tiempo ejecucion crops',t.time()-start_time)
 
 eyes,eye1,eye2,detect_eyes=detect_eyes_OpenCV(ROI_eyes)
+ex1,ey1,ew1,eh1=eye1
+ex2,ey2,ew2,eh2=eye2
+ROI_eye1=eyes[ey1:ey1+eh1 ,ex1:ex1+ew1]
+ROI_eye2=eyes[ey2:ey2+eh2 ,ex2:ex2+ew2]
+am=proy_bin(ROI_eye1)
 cv2.imshow('eyes',eyes)
-print(eye1)
+cv2.imwrite('./capturas/eyes.jpg',ROI_eyes)
+cv2.imwrite('./capturas/eye1.jpg',ROI_eye1)
+cv2.imwrite('./capturas/eye2.jpg',ROI_eye2)
+print('Tamano ojo',ROI_eye1.shape)
 print('Tiempo ejecucion ojos',t.time()-start_time)
 
 cv2.waitKey(0)
