51813c107eff7fe2d8e619a8fdda6a9b959ab826
pedrolo22 <pedro.gpaterna@yahoo.com>
2019-03-08 16:42:09 +0100

    Uso de morfología y aproximación por elipses

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import time as t
+import math
 from matplotlib import pyplot as plt
 #import dlib
 
@@ -82,39 +83,101 @@ def proy_bin(imagen):
 	plt.show()
 	return apertura
 
+def morf_proc(imagen):
+
+	kernel7=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(7,7))
+	kernel8=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(8,8))
+	kernel9=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(9,9))
+	kernel10=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(10,10))
+	kernel11=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11))
+	kernel12=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(12,12))
+	kernel13=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(13,13))
+	kernel14=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(14,14))
+	kernel15=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(15,15))
+	kernel16=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(16,16))
+	kernel17=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(17,17))
+
+	img_rgb=cv2.cvtColor(imagen, cv2.COLOR_GRAY2RGB);
+	img_suav=cv2.GaussianBlur(imagen,(3,3),0)
+	img1=cv2.morphologyEx(img_suav, cv2.MORPH_CLOSE, kernel9)
+	img2=cv2.morphologyEx(img1, cv2.MORPH_CLOSE,kernel15)
+	img3=cv2.subtract(img2,img1)
+	umbral1=np.amax(img3)*0.75
+	ret,img_bw=cv2.threshold(img3, umbral1,255, cv2.THRESH_BINARY)
+	img4=cv2.dilate(img_bw,np.ones(3))
+	img5=cv2.subtract(img4,img_bw)
+	contours, hierarchy = cv2.findContours(img_bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	cv2.drawContours(img_bw, contours, -1, (0,255,0), 1)
+	cnt=contours[0]
+	ellipse=cv2.fitEllipse(cnt)	
+	print(ellipse)
+
+	cv2.imshow('Apertura1',img1)
+	cv2.imshow('Apertura2',img2)
+	cv2.imshow('1-2',img3)
+	cv2.imshow('Umbralizacion',img_bw)
+	cv2.imwrite('./capturas/elipse.png',img_rgb)
+	cv2.imshow('Dilatacion',img4)
+	cv2.imshow('Contorno Pupila', imagen)
+
+	return ellipse
+
+
+
 start_time=t.time()
-im=cv2.imread('images/ojos_entre_HD.jpg',0)
-im_resize=cv2.UMat(cv2.resize(im,(320,180)))
+im_rgb=cv2.imread('images/ojos_abiertos_HD.jpg')
+im_rgb_resize=cv2.resize(im_rgb,(640,360))
+im=cv2.cvtColor(im_rgb,cv2.COLOR_RGB2GRAY)
+im_resize=cv2.UMat(cv2.resize(im,(640,360)))
 
 face_UMat,fx,fy,fw,fh,fdetect=detect_careto_OpenCV(im_resize)
 face=cv2.UMat.get(face_UMat)
-cv2.imshow('pedrolo',im_resize)
-cv2.imwrite('./capturas/im_resize.jpg',face)
+#cv2.imshow('pedrolo',im_resize)
 face_time=t.time()-start_time
 print('Tiempo ejecucion cara',face_time)
 
 ROI_face=face[fy:fy+fh , fx:fx+fw]
-cv2.imshow('face',ROI_face)
-cv2.imwrite('./capturas/face.jpg',ROI_face)
+#cv2.imshow('face',ROI_face)
 dimY,dimX=ROI_face.shape
 print('Tamano cara',dimY,dimX)
 ROI_eyes=ROI_face[int(dimX*0.25):int(dimX*0.55),int(dimY*0.1):int(dimY*0.9)]
 ROI_mouth=ROI_face[int(dimX*0.7):int(dimX*0.95),int(dimY*0.2):int(dimY*0.8)]
-cv2.imshow('mouth', ROI_mouth)
-cv2.imwrite('./capturas/mouth.jpg',ROI_mouth)
+#cv2.imshow('mouth', ROI_mouth)
+
 print('Tamano boca',ROI_mouth.shape)
 print('Tiempo ejecucion crops',t.time()-start_time)
 
 eyes,eye1,eye2,detect_eyes=detect_eyes_OpenCV(ROI_eyes)
 ex1,ey1,ew1,eh1=eye1
 ex2,ey2,ew2,eh2=eye2
-ROI_eye1=eyes[ey1:ey1+eh1 ,ex1:ex1+ew1]
-ROI_eye2=eyes[ey2:ey2+eh2 ,ex2:ex2+ew2]
-am=proy_bin(ROI_eye1)
-cv2.imshow('eyes',eyes)
+expand_eyes=5
+ROI_eye1=eyes[(ey1-expand_eyes):(ey1+eh1+expand_eyes) ,(ex1-expand_eyes):(ex1+ew1+expand_eyes)]
+ROI_eye2=eyes[(ey2-expand_eyes):(ey2+eh2+expand_eyes) ,(ex2-expand_eyes):(ex2+ew2+expand_eyes)]
+
+# Procesamiento morfologico y aproximacion de elipse a pupila
+ellipse=morf_proc(ROI_eye1)
+center,axis,angle=ellipse
+a=float(axis[1])
+b=float(axis[0])
+excentricidad=math.sqrt(a**2-b**2)/a
+center=(center[0]+ex1+fx+dimY*0.1-expand_eyes,center[1]+ey1+fy+dimX*0.25-expand_eyes)
+ellipse=[center,axis,angle]
+cv2.ellipse(im_rgb_resize,tuple(ellipse),(0,255,0),1)
+
+print('Semieje menor',axis[0])
+print('Semieje mayor',axis[1])
+print('Excentricidad', excentricidad)
+cv2.putText(im_rgb_resize, 'Excentricidad= '+ str(excentricidad), (20, 30), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (0,0,0), 1)
+
+
+cv2.imwrite('./capturas/im_resize.jpg',face)
+cv2.imwrite('./capturas/face.jpg',ROI_face)
+cv2.imwrite('./capturas/mouth.jpg',ROI_mouth)
 cv2.imwrite('./capturas/eyes.jpg',ROI_eyes)
 cv2.imwrite('./capturas/eye1.jpg',ROI_eye1)
 cv2.imwrite('./capturas/eye2.jpg',ROI_eye2)
+cv2.imwrite('./capturas/elipse_rgb.png',im_rgb_resize)
+
 print('Tamano ojo',ROI_eye1.shape)
 print('Tiempo ejecucion ojos',t.time()-start_time)
 
